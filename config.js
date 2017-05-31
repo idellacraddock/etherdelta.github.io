b40e4e7abae167055440324be651b422c8c9a8e0
idellacraddock <idella.craddock@gmail.com>
2017-05-31 21:47:23 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /* eslint-env browser */
 
 module.exports = {
-  homeURL: 'https://etherdelta.github.io',
+  homeURL: 'https://idellacraddock.github.io',
   contractEtherDelta: 'smart_contract/etherdelta.sol',
   contractToken: 'smart_contract/token.sol',
   contractReserveToken: 'smart_contract/reservetoken.sol',
